bea9207f0ac63d255c9d756af55e9d19bb712167
Abhishek Guleliya <126302855+AbhishekGuleliya@users.noreply.github.com>
2024-02-23 07:26:07 +0000

    Auto commit on save

--- program1.js
+++ program1.js
@@ -10,6 +10,20 @@ var isValid = function(s) {
         '{': '}'
 };
 
+for (let i = 0; i < s.length; i++) {
+    if (s[i] === '(' || s[i] === '[' || s[i] === '{') {
+        stack.push(s[i]);
+    } else {
+        const top = stack.pop();
+        if (s[i] !== map[top]) {
+            return false;
+        }
+    }
+}
+
+return stack.length === 0;
+};
+
 module.exports = { isValid };
 
 
